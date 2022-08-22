54208d02a14891e83ac2adbb833a0d0b33dddb94
Muhammet Alperen YEŞİLDAĞ <109625708+MuhammetAlperen@users.noreply.github.com>
2022-08-22 16:20:11 +0300

    controller'lar oluşturuldu.

---
+++ TelephoneDirectory/Controllers/PersonController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TelephoneDirectory.Core.Entityes;
+
+namespace TelephoneDirectory.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ TelephoneDirectory/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TelephoneDirectory.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
